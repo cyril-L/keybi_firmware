a9a121d69c223c1ec17e91475596aa2dd8ec99d3
Cyril Lugan <cyril@lugan.fr>
2020-07-13 17:12:30 +0200

    Run unit tests on host

---
+++ src/keybi/tests/stm32f10x.h
@@ -0,0 +1,9 @@
+#pragma once
+
+// Stub header replacing the original for unit tests
+
+#include <stdint.h>
+
+// Normally passed through commandline when compiling
+#define GLOBAL_VID 0x20a0
+#define GLOBAL_PID 0x4108
